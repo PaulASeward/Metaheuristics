5efb62e5c9a3dee8d5b45e62f0ad480870fe9dbf
Paul Seward <110346685+PaulASeward@users.noreply.github.com>
2023-07-06 05:09:53 -0300

    Add files via upload
    Update after first successful complete run

--- Basic optimization algorithms/pso/pso_run.py
+++ Basic optimization algorithms/pso/pso_run.py
@@ -1,5 +1,6 @@
-from pso.Standard_PSO import *
+from Standard_PSO import *
 from Vectorized_PSOGlobalLocal import *
+# from functions import *
 import time
 
 # Fitness of the global optima for the 28 functions of the CEC'13 benchmark
@@ -7,64 +8,59 @@ fDeltas = [-1400, -1300, -1200, -1100, -1000, -900, -800, -700, -600,
            -500, -400, -300, -200, -100, 100, 200, 300, 400, 500, 600,
            700, 800, 900, 1000, 1100, 1200, 1300, 1400]
 
-
 def run_experiment(runs, dimensions, evals_per_dim, swarm_size, global_topology, vectorized):
-    function_result_array = []
-    run_results = []
     obj_f = functions.CEC_functions(dimensions)
-    topology_label = (lambda: 'Global' if global_topology else 'Local')()
-    comp_label = (lambda: 'Vectorized' if vectorized else 'Iterative')()
 
-    for fun_num in range(1, 6):
-        error_list = []  # Store errors for each run of the current function
-        run_result = []
-        total_time = 0.0
-
-        for run in range(1, runs+1):
-            # print(f"Function {fun_num}. Run Number: {run}/{runs}. Topology: {topology_label}. Vectorized: {vectorized}")
-
-            if vectorized:
-                pso = PSOVectorSwarmGlobalLocal(obj_f, fun_num, dimensions, evals_per_dim, swarm_size, global_topology=global_topology)
-            else:  # Standard Approach (Non-Vectorized)
-                pso = ParticleSwarmOptimizer(fun_num)
-
-            start_time = time.time()  # Start timing
-            gbest_val, gbest_pos = pso.optimize()
-            end_time = time.time()  # End timing
-
-            run_time = end_time - start_time  # Calculate the time taken for this run
-            error = gbest_val - fDeltas[fun_num - 1]  # Calculate the error for this run.
-
-            error_list.append(error)  # Add error to the list
-            run_result.append([fun_num, run, error, run_time])  # Store result for this run
-
-            total_time += run_time  # Accumulate the total time
-
-            # print(f"Current result (error respect to the global optimum): {error:.2E}")
-            # print(f"Time taken for this run: {run_time:.2f} seconds")
-
-        avg_error = np.sum(error_list) / runs
-        avg_time = total_time / runs
-        std_dev = np.std(error_list)  # Calculate standard deviation
-
-        # print(f"Function {fun_num}, result (Average error respect to the global optimum): {avg_error:.2E}")
-        # print(f"Average time taken: {avg_time:.2f} seconds")
-        run_results.append(run_result)
-        function_result_array.append([fun_num, avg_error, std_dev, avg_time])  # Include std_dev in the result array
+    topology_label = 'Global' if global_topology else 'Local'
+    comp_label = 'Vectorized' if vectorized else 'Iterative'
 
     # Save run results to a CSV file
     with open(f'{topology_label}{comp_label}runresults.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Function Number', 'Run Number', 'Error', 'Time'])  # Header for run results
-        for run_result in run_results:
-            writer.writerows(run_result)  # Write each run result
 
-    # Save function number summative results to a CSV file
     with open(f'{topology_label}{comp_label}functionresults.csv', 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(['Function Number', 'Mean Error', 'Standard Deviation', 'Mean Time'])  # Header for result array
-        writer.writerows(function_result_array)  # Write result array
 
+    for fun_num in range(1, 29):
+        error_list = []
+        run_results = []
+        total_time = 0.0
+
+        for run in range(1, runs+1):
+            # print(f"Function {fun_num}. Run Number: {run}/{runs}. Topology: {topology_label}. Vectorized: {vectorized}")
+            if vectorized:
+                pso = PSOVectorSwarmGlobalLocal(obj_f, fun_num, dimensions, evals_per_dim, swarm_size, global_topology=global_topology)
+            else:
+                pso = ParticleSwarmOptimizer(fun_num)
+
+            start_time = time.time()
+            gbest_val, gbest_pos = pso.optimize()
+            end_time = time.time()
+
+            run_time = end_time - start_time
+            error = gbest_val - fDeltas[fun_num - 1]
+
+            error_list.append(error)
+            run_results.append([fun_num, run, error, run_time])
+
+            total_time += run_time
+
+            # Write current result to CSV file
+            with open(f'{topology_label}{comp_label}runresults.csv', 'a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([fun_num, run, error, run_time])
+
+        avg_error = np.sum(error_list) / runs
+        avg_time = total_time / runs
+        std_dev = np.std(error_list)
+
+        function_result_array = [[fun_num, avg_error, std_dev, avg_time]]
+        # Write function number summative result to CSV file
+        with open(f'{topology_label}{comp_label}functionresults.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([fun_num, avg_error, std_dev, avg_time])
 
 runs = 30
 dimensions = 30
